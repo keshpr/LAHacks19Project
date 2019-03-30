771d3c7437a202dad586601bae48c3a37469999d
Satyam Gupta <satyamgupta19946@gmail.com>
2019-03-30 14:31:06 -0700

    Update UnitScript.cs
    Changed and added UnitScript.cs

--- Assets/Scripts/UnitScript.cs
+++ Assets/Scripts/UnitScript.cs
@@ -5,15 +5,44 @@ using UnityEngine;
 public class UnitScript : MonoBehaviour
 {
 
+    public GameObject[] resources;
+    public float[] decrementRate;
+    public float[] initAmount;
+    public float[] currentAmount;
+    private int numResources;
+    
+    GameControllerScript gameController;
+    
     // Start is called before the first frame update
     void Start()
     {
+        numResources = currentAmount.Length;
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameControllerScript>();
         
+        for ( int i = 0; i < numResources; i++ ){
+            currentAmount[i] = initAmount[i];
+        }
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        for ( int i = 0; i < numResources; i++ )
+            currentAmount[i] -= decrementRate[i] * Time.deltaTime;
     }
+    
+    //If Mouse pressed on the object check what is the currentActiveResource and if it matches then remake the value of currentAmount.
+    void onMouseDown(){
+        GameObject activeResource = gameController.getActiveResource();
+        if ( activeResource == null ){
+            return;
+        }
+        
+        for ( int i = 0; i < numResources; i++ ){
+            if ( activeResource.tag == resources[i].tag ){
+                currentAmount[i] = initAmount[i];
+            }
+        }
+    }
+
 }
