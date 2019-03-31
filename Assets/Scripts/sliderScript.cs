9833730b8775e400bf6bb038e3504e7345440383
Tim <tbingtan@uci.edu>
2019-03-30 22:05:53 -0700

    Updated UI with sliders

---
+++ Assets/Scripts/sliderScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderScript : MonoBehaviour
+{
+    public int number;
+    public Slider slider;
+    public string meterName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = number;
+        // update with meterScript simulatenously...
+    }
+
+    void setSliderValue(int sliderValue)
+    {
+        number = sliderValue;
+    }
+
+}
