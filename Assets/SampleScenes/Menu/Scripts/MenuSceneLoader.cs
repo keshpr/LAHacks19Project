25410baeeb0f725cdc5b49f8d829f4152deab461
Lilit Danelyan <lilitdanelyan@g.ucla.edu>
2019-03-30 23:26:41 -0700

    menu scene with buttons but buttons not working

--- Assets/SampleScenes/Menu/Scripts/MenuSceneLoader.cs
+++ Assets/SampleScenes/Menu/Scripts/MenuSceneLoader.cs
@@ -1,17 +1,11 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuSceneLoader : MonoBehaviour
 {
-    public GameObject menuUI;
-
-    private GameObject m_Go;
-
-	void Awake ()
-	{
-	    if (m_Go == null)
-	    {
-	        m_Go = Instantiate(menuUI);
-	    }
-	}
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
